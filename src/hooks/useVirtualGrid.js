4f1f986406f777820ff50be503b4c582a1f1672b
Adam Skoog <adamskoog@outlook.com>
2022-04-07 06:24:13 -0500

    Update src/hooks/useVirtualGrid.js
    Co-authored-by: Thomas Colin <th.colin3@gmail.com>

--- src/hooks/useVirtualGrid.js
+++ src/hooks/useVirtualGrid.js
@@ -5,7 +5,7 @@ import useVirtualGridChildren from './useVirtualGridChildren'
 const useVirtualGrid = ({ cell, total, viewportRowOffset = 4, onRender, scrollContainer }) => {
   const rowOffset = Math.max(2, Math.round(viewportRowOffset / 2) * 2)
   const { display, style, ref } = useVirtualGridDisplay({ cell, total, rowOffset })
-  const { firstRowIndex, scrolling } = useVirtualGridFirstRowIndex({ ...display, scrollContainer })
+  const { firstRowIndex, scrolling } = useVirtualGridFirstRowIndex(display, scrollContainer)
   const children = useVirtualGridChildren({ firstRowIndex, scrolling, display, onRender })
 
   return { container: { ref, style }, children }
