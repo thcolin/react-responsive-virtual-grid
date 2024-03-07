76a1c5aa17f79c7bd48b09a8725fe022f4ac7888
thcolin <th.colin3@gmail.com>
2024-03-07 14:55:19 +0100

    Fix layout.top value recompute issue on `cell`, `total` or `rowOffset` props change
    * When `cell`, `total` or `rowOffset` props change, if user already scrolled into the virtual list, `layout.top` is recomputed with new `windows.scrollY` value, which cancel `position` compute on `useVirtualGridFirstRowIndex` hook

--- src/hooks/useVirtualGridDisplay.js
+++ src/hooks/useVirtualGridDisplay.js
@@ -6,6 +6,7 @@ import { BROWSER_PX_VALUE_LIMIT } from '../constants'
 
 const useVirtualGridDisplay = ({ cell, total, rowOffset }) => {
   const isClient = typeof window === 'object'
+  // const initialScrollY = useMemo(() => isClient ? window.scrollY : 0, [])
   const viewport = useWindowSize()
   const [ref, bounds] = useMeasure(isClient && window.ResizeObserver ? {} : { polyfill: ResizeObserver })
   const [initial, setInitial] = useState({ width: 0, top: 0 })
@@ -23,7 +24,7 @@ const useVirtualGridDisplay = ({ cell, total, rowOffset }) => {
     columns.total = cell.width ? Math.floor(layout.width / cell.width) : 1
     rows.total = Math.ceil(total / columns.total)
     rows.total = Math.min(rows.total, Math.floor(BROWSER_PX_VALUE_LIMIT / cell.height))
-    layout.top = Math.floor((bounds.top || initial.top) + (isClient ? window.scrollY : 0))
+    layout.top = Math.floor((bounds.top || initial.top) /* + initialScrollY */)
     layout.height = rows.total * cell.height
     columns.height = layout.height
     columns.width = Math.floor(layout.width / Math.max(1, columns.total))
@@ -65,6 +66,7 @@ const useVirtualGridDisplay = ({ cell, total, rowOffset }) => {
     bounds.top,
     initial.width,
     initial.top,
+    // initialScrollY,
   ])
 
   return { display, style, ref }
